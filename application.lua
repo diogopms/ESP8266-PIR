d2be0d98318082ee982cd685346b91fd4d21116b
Diogo Serrano <diogo.serrano@talkdesk.com>
2016-11-27 21:29:54 +0000

    Add suport for Pushover API
    Add support for Pushover API.
    Read more information in http://pushover.net

--- application.lua
+++ application.lua
@@ -6,16 +6,21 @@ gpio.mode(LedPin,gpio.OUTPUT)
 PirPin=4 -- GPIO 2
 gpio.mode(PirPin,gpio.INT,gpio.PULLUP)
 
+local pushover = require('pushover')
+
 function motion()
-  print("Motion Detection : ON!")
+  message = "Motion Detection : ON"
+  print(message)
   stat = "ON"
   gpio.write(LedPin,gpio.HIGH)  -- Led ON - Motion detected
   gpio.trig(PirPin,"down",nomotion)  -- trigger on falling edge
+  pushover.postData("Alert", message)
   return stat
 end
 
 function nomotion()
-  print("Motion Detection : OFF!")
+  message = "Motion Detection : OFF"
+  print(message)
   stat = "OFF"
   gpio.write(LedPin,gpio.LOW)   -- Led OFF
   gpio.trig(PirPin,"up",motion)  -- trigger on rising edge
---
+++ pushover.lua
@@ -0,0 +1,40 @@
+-- pushover.lua
+
+local pushover = {}
+
+PUSHVOER_IP="108.59.13.232"
+PUSHOVER_HOST="api.pushover.net"
+PUSHOVER_PATH="/1/messages.json"
+PUSHOVER_TOKEN="XXXX"
+PUSHOVER_USER="XXXX"
+
+function pushover.postData(title, message)
+  if title == nil or message == nil then
+    return
+  end
+  postToServer("token="..PUSHOVER_TOKEN.."&user="..PUSHOVER_USER.."&title="..title.."&message="..message)
+end
+
+function getConnection()
+  local conn = net.createConnection(net.TCP, 0)
+  conn:on("sent", function(conn) conn:close() end)
+  return conn
+end
+
+function postToServer(readingData)
+
+  local conn = getConnection()
+
+  conn:connect("80", PUSHVOER_IP)
+  conn:send("POST " .. PUSHOVER_PATH .. " HTTP/1.1\r\n")
+  conn:send("Host: " .. PUSHOVER_HOST .. "\r\n")
+  conn:send("Accept: */*\r\n")
+  conn:send("Connection: close\r\n")
+  conn:send("Content-Length: " .. string.len(readingData) .. "\r\n")
+  conn:send("Content-Type: application/x-www-form-urlencoded\r\n")
+  conn:send("User-Agent: ESP8266 (compatible; esp8266 Lua;)\r\n")
+  conn:send("\r\n")
+  conn:send(readingData)
+end
+
+return pushover
